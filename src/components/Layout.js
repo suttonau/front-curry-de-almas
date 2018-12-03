7b492995272e0afa8205f6839114ec94a8a38496
suttonau <sutton_mjbc@icloud.com>
2018-12-03 17:20:19 -0500

    setting up Layout Component

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Aux from '../hoc/Aux'
+
+const Layout = (props) => (
+  <Aux>
+    <div>
+    Toolbar,
+    Sidedrawer,
+    Backdrop
+    </div>
+    <main>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default Layout;
---
+++ src/containers/OrderBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Aux from '../hoc/Aux';
+
+class OrderBuilder extends Component {
+
+
+
+  render() {
+    return (
+      <Aux>
+        <div>Item</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default OrderBuilder;
